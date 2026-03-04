021be595341e86d305abd599e093d8bdaf0180ea
skempfer <shana.kempfer88@gmail.com>
2026-03-04 20:51:06 -0300

    Add exercise 3.4 e 3.5

--- part3/index.js
+++ part3/index.js
@@ -26,6 +26,18 @@ app.get('/info', (req, res) => {
   `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
