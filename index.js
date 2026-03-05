984b7691de75c05a31aab3cfc71fb2701128f5a0
skempfer <shana.kempfer88@gmail.com>
2026-03-04 22:18:39 -0300

    Add exercise 3.11

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const morgan = require('morgan')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const morgan = require('morgan')
+
+app.use(cors())
 
 app.use(express.json())
 app.use(morgan('tiny'))
