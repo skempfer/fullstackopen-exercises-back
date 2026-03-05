71c91538d97c6b49e7960c6f300768efced4173a
skempfer <shana.kempfer88@gmail.com>
2026-03-05 14:24:42 -0300

    Add exercise 3.12

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose');
+const Person = require('./models/Person');
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
@@ -23,49 +27,46 @@ app.use(
 
 const PORT = process.env.PORT || 3001
 
-let persons = [
-  { id: 1, name: "Arto Hellas", number: "040-123456" },
-  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
-]
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch(err => console.error('error connecting to MongoDB:', err));
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
-  const total = persons.length
-  const date = new Date()
+  Person.countDocuments({}).then(count => {
+    const total = count
+    const date = new Date()
 
-  res.send(`
-    <p>Phonebook has info for ${total} people</p>
-    <p>${date}</p>
-  `)
+    res.send(`
+      <p>Phonebook has info for ${total} people</p>
+      <p>${date}</p>
+    `)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  }).catch(error => {
+    res.status(400).send({ error: 'malformatted id' })
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-
-  persons = persons.filter(p => p.id !== id)
-
-  res.status(204).end()
+  Person.findByIdAndDelete(req.params.id).then(() => {
+    res.status(204).end()
+  }).catch(error => {
+    res.status(400).send({ error: 'malformatted id' })
+  })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -77,23 +78,28 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  const nameExists = persons.some(p => p.name === body.name)
+  Person.findOne({ name: body.name }).then(existingPerson => {
+    if (existingPerson) {
+      return res.status(400).json({
+        error: 'name must be unique'
+      })
+    }
 
-  if (nameExists) {
-    return res.status(400).json({
-      error: 'name must be unique'
+    const person = new Person({
+      name: body.name,
+      number: body.number
     })
-  }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 10000),
-    name: body.name,
-    number: body.number
-  }
+    return person.save()
+  }).then(savedPerson => {
+    res.json(savedPerson)
+  }).catch(error => {
+     if (error.name === 'MongoServerError' && error.code === 11000) {
+      return res.status(400).json({ error: 'name must be unique' })
+    }
 
-  persons = persons.concat(newPerson)
-
-  res.json(newPerson)
+    res.status(400).json({ error: error.message })
+  })
 })
 
 app.use((req, res) => {
@@ -108,6 +114,10 @@ app.use((err, req, res, next) => {
   }
 
   next(err)
+})
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 app.listen(PORT, () => {
