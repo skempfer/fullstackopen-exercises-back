f0c90c4b74fccb171dcc5e7be6432e9f8e1ccef5
skempfer <shana.kempfer88@gmail.com>
2026-03-17 20:43:01 -0300

    fix login issue

--- part4/app.js
+++ part4/app.js
@@ -1,19 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const config = require('./utils/config')
 const middleware = require('./utils/middleware')
-
-if (mongoose.connection.readyState === 0) {
-	if (!config.MONGODB_URI) {
-		throw new Error('MONGODB_URI or TEST_MONGODB_URI is not defined')
-	}
-
-	mongoose.connect(config.MONGODB_URI)
-}
 
 const app = express()
 
