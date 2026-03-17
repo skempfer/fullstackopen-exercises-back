fb964f8d350bed8b7664c4787655f5e02f3c392a
skempfer <shana.kempfer88@gmail.com>
2026-03-17 11:43:02 -0300

    Add exercises 4.9

--- part4/app.js
+++ part4/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blog')
+const config = require('./utils/config')
+
+if (mongoose.connection.readyState === 0) {
+	if (!config.MONGODB_URI) {
+		throw new Error('MONGODB_URI or TEST_MONGODB_URI is not defined')
+	}
+
+	mongoose.connect(config.MONGODB_URI)
+}
 
 const app = express()
 
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,8 +1,43 @@
 const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
 const supertest = require('supertest')
 
-const app = require('../app')
-const api = supertest(app)
+const Blog = require('../models/blog')
+
+jest.setTimeout(30000)
+
+let mongoServer
+let api
+
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Alice',
+    url: 'https://example.com/1',
+    likes: 1
+  },
+  {
+    title: 'Second blog',
+    author: 'Bob',
+    url: 'https://example.com/2',
+    likes: 2
+  }
+]
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create()
+  process.env.NODE_ENV = 'test'
+  process.env.TEST_MONGODB_URI = mongoServer.getUri()
+
+  await mongoose.connect(process.env.TEST_MONGODB_URI)
+  const app = require('../app')
+  api = supertest(app)
+})
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
 
 
 test('blogs are returned as json', async () => {
@@ -12,6 +47,27 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('unique identifier is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  const blog = response.body[0]
+
+  expect(blog.id).toBeDefined()
+  expect(blog._id).toBeUndefined()
+})
+
+test('blogs have id property', async () => {
+  const response = await api.get('/api/blogs')
+
+  const blog = response.body[0]
+
+  expect(blog.id).toBeDefined()
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
+
+  if (mongoServer) {
+    await mongoServer.stop()
+  }
 })
